6bb9a56f88e9757088f15f52afc981ddb98f826d
MOHD AMIR <mohdamirwebdeveloper@gmail.com>
2025-04-06 20:32:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import simulator
+
+while True:
+    # A = int(input("Enter the first Binary bit : "))
+    # B = int(input("Enter the Second Binary bit : "))
+    # C = int(input("Enter the Third Binary bit : "))
+    # D = int(input("Enter the Third Binary bit : "))
+    
+    # A1 = int(input("Enter the first Binary bit : "))
+    # B1 = int(input("Enter the Second Binary bit : "))
+    # C1 = int(input("Enter the Third Binary bit : "))
+    # D1 = int(input("Enter the Third Binary bit : "))
+    
+    simulator.FullAdder()
+    break
