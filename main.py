0052fb69e55d76833e6ecc78b7b1f6acb0704cad
MOHD AMIR <mohdamirwebdeveloper@gmail.com>
2025-04-07 21:51:42 +0530

    Update main.py
    Added examples implemented a Full adder using MUX

--- main.py
+++ main.py
@@ -13,4 +13,16 @@ while True:
     
     x = simulator.Mux4x1(1,1,0,1,0,0)
     print(x)
-    break
+
+        #examples
+    #Implementing Full adder using MUX
+    A = int(input("Enter the first Bit A : "))
+    B = int(input("Enter the Second Bit B : "))
+    Cin = int(input("Enter the Third Bit Cin (Carry IN) : "))
+    
+    Acomp = simulator.NOT(A)
+
+    sum = simulator.Mux4x1(A,Acomp,Acomp,A,B,Cin)
+    carry = simulator.Mux4x1(0,A,A,1,B,Cin)
+    print(sum,carry)
+    
